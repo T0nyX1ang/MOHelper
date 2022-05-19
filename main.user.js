3c299250dd7bf6c051f90bca00c0f3f8d89c11a3
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2022-05-19 21:39:00 +0800

    Update main.user.js

--- main.user.js
+++ main.user.js
@@ -7,7 +7,7 @@
 // @license      AGPL-3.0
 // @match        https://minesweeper.online/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
-// @require      https://github.com/T0nyX1ang/MOHelper/raw/main/solver.js
+// @require      https://github.com/T0nyX1ang/MOHelper/raw/master/solver.js
 // @grant        none
 // ==/UserScript==
 
